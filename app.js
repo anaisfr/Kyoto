f1e0907390ac79481186c8bb6868dca933534f72
Anais Francois <anaisfrncs@gmail.com>
2021-04-20 19:00:22 +0200

    fixed pbm navbar

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const btnMenu = document.querySelector('.logo-menu');
 const menu = document.querySelector('.liste-nav');
 
 btnMenu.addEventListener('click', function(){
-    menu.classList.toggle('.active');
+    menu.classList.toggle('active');
 })
